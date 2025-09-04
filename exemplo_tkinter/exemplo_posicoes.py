1b370600b7f1eb7aab210296bbe53b9b5e2ddd82
Tiago-Fogolin <ticofogolin@gmail.com>
2025-09-04 13:08:51 -0300

    Exibir posição ao clicar na tela

---
+++ exemplo_tkinter/exemplo_posicoes.py
@@ -0,0 +1,41 @@
+import customtkinter as ctk
+
+
+janela = ctk.CTk()
+janela.geometry("800x600")
+janela.title("Titulo da Janela")
+
+
+label = ctk.CTkLabel(janela, width=200, height=50, text="Primeiro Texto", font=("Arial", 20), text_color="red")
+label.place(x=300, y=10)
+
+entrada = ctk.CTkEntry(
+                        janela, 
+                        width=200, 
+                        height=20, 
+                        placeholder_text="Escreva seu nome", 
+                        font=("Arial", 14)
+                    )
+entrada.place(x=300, y=100)
+
+
+def mostrar_nome():
+    nome = entrada.get()
+
+    label_nome.configure(text=f"Olá, {nome}!")
+    label_nome.place(x=300, y=300)
+
+    print(nome)
+
+botao = ctk.CTkButton(janela, width=100, height=50, text="Me clica", command=mostrar_nome)
+botao.place(x=350, y=200)
+
+label_nome = ctk.CTkLabel(janela, width=200, height=50, font=("Arial", 20))
+
+def mostrar_posicao(event):
+    print(f"{event.x}, {event.y}")
+
+janela.bind("<Button-1>", mostrar_posicao)
+
+
+janela.mainloop()
